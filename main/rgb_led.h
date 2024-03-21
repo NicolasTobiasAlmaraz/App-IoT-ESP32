a7b1d7d48a6e1b3753d230eb0027ffda2d551611
NicolasTobiasAlmaraz <nnico.almaraz@gmail.com>
2024-03-21 00:00:48 -0300

    Agrego funcionalidades de led RGB

---
+++ main/rgb_led.h
@@ -0,0 +1,50 @@
+/*
+ * rgb_led.h
+ *
+ *  Created on: MAR 20, 2024
+ *
+ *  Author: Nicolás
+ *
+ *  Brief: En este archivo se definen los puertos utilizados para cada color,
+ *  el número de canales, la configuración de la estructura y los hardcoding de cada
+ *  color
+ *
+ */
+
+#ifndef MAIN_RGB_LED_H_
+#define MAIN_RGB_LED_H_
+
+// RGB LED GPIOs
+#define RGB_LED_RED_GPIO		21
+#define RGB_LED_GREEN_GPIO		19
+#define RGB_LED_BLUE_GPIO		18
+
+// RGB LED color mix channels
+#define RGB_LED_CHANNEL_NUM		3
+
+// RGB LED configuration
+typedef struct
+{
+	int channel;
+	int gpio;
+	int mode;
+	int timer_index;
+} ledc_info_t;
+// ledc_info_t ledc_ch[RGB_LED_CHANNEL_NUM]; Move this declaration to the top of rgb_led.c to avoid linker errors
+
+/**
+ * Color to indicate WiFi application has started.
+ */
+void rgb_led_wifi_app_started(void);
+
+/**
+ * Color to indicate HTTP server has started.
+ */
+void rgb_led_http_server_started(void);
+
+/**
+ * Color to indicate that the ESP32 is connected to an access point.
+ */
+void rgb_led_wifi_connected(void);
+
+#endif /* MAIN_RGB_LED_H_ */
